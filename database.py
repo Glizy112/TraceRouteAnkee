92cf86ec4a9c17d8c766ede7506ebdc916a0f2ed
Glizy112 <gargakash098@gmail.com>
2026-07-25 15:16:42 +0530

    DB and Configs

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
 import sqlite3
+import init_db
 
-DATABASE = "database/traceroute.db"
+DATABASE = "/tmp/traceroute.db"
 
 def get_db_connection():
     #conn = get_db_connection()
     conn = sqlite3.connect(DATABASE)
+    init_db()
     conn.row_factory = sqlite3.Row
     return conn
--- init_db.py
+++ init_db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("database/traceroute.db")
+conn = sqlite3.connect("/tmp/traceroute.db")
 
 cursor = conn.cursor()
 cursor.execute("""
